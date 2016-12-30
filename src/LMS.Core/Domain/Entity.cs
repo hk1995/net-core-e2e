47523cf2eb2ec660fa328eaeaff25cd801b75a34
Trevor Chunestudy <trevorchunestudy@validationllc.com>
2016-12-30 12:43:51 -0800

    Added initial entity User

---
+++ src/LMS.Core/Domain/Entity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LMS.Core.Domain
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            CreatedOn = DateTime.UtcNow;
+        }
+
+        public long Id { get; set; }
+        public DateTime CreatedOn { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public void SoftDelete()
+        {
+            IsDeleted = true;
+        }
+
+        public void UnDelete()
+        {
+            IsDeleted = false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+    }
+}
---
+++ src/LMS.Core/Domain/Management/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Core.Domain.Management
+{
+    public class User : Entity
+    {
+        private User(string authId)
+        {
+            AuthId = authId;
+        }
+
+        //Needed for EF
+        private User () { }
+
+        [MaxLength(100)]
+        public string AuthId { get; private set; }
+
+        public User Create(string authId)
+        {
+            return new User(authId);
+        }
+    }
+}
