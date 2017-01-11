4be1b6895878e023e68ffa14c17cc58c233cd4d9
Trevor Chunestudy <trevorchunestudy@validationllc.com>
2017-01-11 11:50:17 -0800

    Integration testing setup completed.

---
+++ src/Testing/LMS.Integration.Tests/DeleteData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Fixie;
+
+namespace LMS.Integration.Tests
+{
+    public class DeleteData : FixtureBehavior, ClassBehavior
+    {
+        public void Execute(Class context, Action next)
+        {
+            BaseSliceFixture.ResetCheckpoint();
+            next();
+        }
+
+        public void Execute(Fixture context, Action next)
+        {
+            BaseSliceFixture.ResetCheckpoint();
+            next();
+        }
+    }
+}
