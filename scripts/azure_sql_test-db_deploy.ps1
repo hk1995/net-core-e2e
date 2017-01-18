fb1035b243fa9180b54f690bf4875325536de379
Trevor Chunestudy <trevorchunestudy@validationllc.com>
2017-01-18 09:14:45 -0800

    .ps1 13

--- scripts/azure_sql_test-db_deploy.ps1
+++ scripts/azure_sql_test-db_deploy.ps1
@@ -1,9 +1,12 @@
+Write-Host "Setting credentials"
 $resourceGroupName = "VAL"
 $sqlServerName = "val"
-
-$databaseName = "bing-bong"
+$databaseName = "bing-bongg"
 $databaseEdition = "Basic"
 
+Write-Host "Creating a $databaseEdition database $databaseName on SQL Server $databaseName"
 $currentDatabase = New-AzureRmSqlDatabase -ResourceGroupName $resourceGroupName `
  -ServerName $sqlServerName -DatabaseName $databaseName `
  -Edition $databaseEdition 
+
+Write-Host "databaseName database created"
