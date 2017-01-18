25062fb4e382cf9926cdf8f48e758ac9d8cf7e13
Trevor Chunestudy <trevorchunestudy@validationllc.com>
2017-01-18 08:07:21 -0800

    .ps1 10

--- scripts/azure_sql_test-db_deploy.ps1
+++ scripts/azure_sql_test-db_deploy.ps1
@@ -1,12 +1,12 @@
 Write-Host "Setting credentials"
-$serverAdmin = "trevor-admin"
-$serverPassword = "U{%k?aQ'J23/FBhM"
-$creds = new-object System.Management.Automation.PSCredential($serverAdmin, ($serverPassword | ConvertTo-SecureString -asPlainText -Force))
+$creds = new-object System.Management.Automation.PSCredential("trevor-admin", ("U{%k?aQ'J23/FBhM"  | ConvertTo-SecureString -asPlainText -Force))
 
 Write-Host "Setting db context"
 $context = New-AzureSqlDatabaseServerContext -ServerName "val" -Credential $creds
 
+Get-AzureSqlDatabaseServerContext
+
 Write-Host "Creating new test database 'bing-bong'"
-$currentDatabase = New-AzureSqlDatabase -DatabaseName "bing-bong" -Edition "Basic" -ConnectionContext $context
+New-AzureSqlDatabase $context -DatabaseName bing-bong -Edition Basic
 
 Write-Host "'bing-bong' deployed. Done"
